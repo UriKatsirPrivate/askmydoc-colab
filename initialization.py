5766b4961fa7c23f790745d1bd104a619fada2fc
Uri Katsir <46541339+UriKatsirPrivate@users.noreply.github.com>
2023-09-21 17:23:22 +0300

    using args

--- initialization.py
+++ initialization.py
@@ -2,16 +2,18 @@ import os
 import json
 import vertexai
 from langchain.llms import VertexAI
+import sys
 # from google.cloud import secretmanager
 
 
 def initialize_llm():
-    vertexai.init(project="landing-zone-demo-341118", location="us-central1")
+    # vertexai.init(project="landing-zone-demo-341118", location="us-central1")
+    vertexai.init(project=sys.argv[1], location=sys.argv[2])
     return VertexAI(
-        model_name="text-bison",
-        max_output_tokens=1024,
-        temperature=0.1,
-        top_p=0.8,
-        top_k=40,
+        model_name=sys.argv[3],
+        max_output_tokens=sys.argv[4],
+        temperature=sys.argv[5],
+        top_p=sys.argv[6],
+        top_k=sys.argv[7],
         verbose=True,
     )
