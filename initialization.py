d5d83614688996868c2e9b14c243f6a8834690ad
Uri Katsir <46541339+UriKatsirPrivate@users.noreply.github.com>
2023-09-22 18:43:14 +0300

    Added the ability to run without external input

--- initialization.py
+++ initialization.py
@@ -3,17 +3,33 @@ import json
 import vertexai
 from langchain.llms import VertexAI
 import sys
-# from google.cloud import secretmanager
 
+# print ("Number of arguments:", len(sys.argv), "arguments")
+# print ("Argument List:", str(sys.argv))
 
 def initialize_llm():
-    # vertexai.init(project="landing-zone-demo-341118", location="us-central1")
-    vertexai.init(project=sys.argv[1], location=sys.argv[2])
-    return VertexAI(
-        model_name=sys.argv[3],
-        max_output_tokens=sys.argv[4],
-        temperature=sys.argv[5],
-        top_p=sys.argv[6],
-        top_k=sys.argv[7],
-        verbose=True,
-    )
+
+    if len(sys.argv)<=1: # No external passed
+        vertexai.init(project="landing-zone-demo-341118", location="us-central1")
+        return VertexAI(
+            model_name="text-bison@001",
+            max_output_tokens=1024,
+            temperature=0.1,
+            top_p=0.8,
+            top_k=40,
+            verbose=True,
+        )
+    else:     
+        vertexai.init(project=sys.argv[1], location=sys.argv[2])
+        return VertexAI(
+            model_name=sys.argv[3],
+            max_output_tokens=sys.argv[4],
+            temperature=sys.argv[5],
+            top_p=sys.argv[6],
+            top_k=sys.argv[7],
+            verbose=True,
+        )
+
+
+    
+    
--- run-venv.sh
+++ run-venv.sh
@@ -3,16 +3,16 @@ source myenv/bin/activate
 python3 -m pip install --upgrade pip
 export SYSTEM_VERSION_COMPAT=1
 pip3 install -r requirements.txt
-streamlit run app.py "landing-zone-demo-341118"  \
-                                                "us-central1"  \
-                                                "text-bison"  \
-                                                1024 \
-                                                0.1 \
-                                                0.8 \
-                                                40 \
+# streamlit run app.py "landing-zone-demo-341118"  \
+#                                                 "us-central1"  \
+#                                                 "text-bison"  \
+#                                                 1024 \
+#                                                 0.1 \
+#                                                 0.8 \
+#                                                 40 \
 
 
-# streamlit run app.py --server.port 8081 --browser.serverAddress 0.0.0.0 --server.enableCORS False
+streamlit run app.py --server.port 8081 --browser.serverAddress 0.0.0.0 --server.enableCORS False
 # python3 back_sql.py
 # deactivate
 # rm -rf myenv
